5b5741288304043140a7cff2a68f06df31b18921
Fernando López Moreno <koficoud@gmail.com>
2022-04-11 05:35:39 -0500

    Install GTM.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import TagManager from 'react-gtm-module';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-PLTD4SW' });
+  }, []);
+
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
